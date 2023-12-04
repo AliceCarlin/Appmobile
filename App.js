d4a035c6cc51611d160f87c8d0574ef47026da16
AliceCarlin <alice.carlin@free.fr>
2023-12-04 15:32:37 +0100

    premiers tests avec l'environnement, installation d'axios

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+const CharacterDisplay = () => {
+  const [characterData, setCharacterData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://rickandmortyapi.com/api/character/2');
+        setCharacterData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.boxes}>
+        <Text style={{color: '#fff'}}>heyyy</Text>
+      </View>
+      <View style={styles.boxes}>
+        <Text>Character Name: {characterData.name}</Text>
+      </View>
+      <View style={styles.boxes}>
+        
+      </View>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +44,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boxes:{
+    height: 72,
+    width: 320,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    marginBottom:5,
+    marginTop: 5,
+    borderRadius: 15,
+  },
+  text:{
+    color:'red'
+  }
 });
